2ec4f099aee4a1803f973901239b06ef2e33ada1
Clean18 <cleanage95@gmail.com>
2025-06-12 10:00:32 +0900

    Feat : 피칭머신
    공 속도 및 오브젝트풀 적용해야함

---
+++ Assets/Scripts/PitchingMachine.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitchingMachine : MonoBehaviour
+{
+    public Transform ballSpawnPosition;
+	public GameObject ballPrefab;
+
+	public float shootDelayTime = 1f;
+	public float shootPower = 50f;
+    Coroutine shootRoutine;
+	WaitForSeconds shootDelay;
+
+	void Start()
+	{
+		shootDelay = new WaitForSeconds(shootDelayTime);
+	}
+
+	void Update()
+	{
+		if (shootRoutine == null)
+		{
+			shootRoutine = StartCoroutine(Shoot());
+		}
+	}
+
+	IEnumerator Shoot()
+	{
+		while (true)
+		{
+			// 정면 방향으로 발사
+			GameObject ball = Instantiate(ballPrefab, ballSpawnPosition.position, ballSpawnPosition.rotation);
+			Rigidbody rigid = ball.GetComponent<Rigidbody>();
+
+			// AddForce
+			//rigid.AddForce(ballSpawnPosition.forward * shootPower, ForceMode.Impulse); // 너무빠름
+
+			// Velocity
+			rigid.velocity = ballSpawnPosition.forward * shootPower; // 느린데 포물선그림
+
+			yield return shootDelay;
+		}
+	}
+}
