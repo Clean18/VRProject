c7b2a9f17acd3a8e781997e8465cc41b4f299ec9
Clean18 <cleanage95@gmail.com>
2025-06-12 09:59:40 +0900

    Create : Baseball Field, Teleport Setting
    야구장 생성 및 텔레포트 설정

---
+++ Assets/Scripts/CustomGrabInteractable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CustomGrabInteractable : XRGrabInteractable
+{
+	void Start()
+	{
+		
+	}
+
+	protected override void OnSelectEntered(SelectEnterEventArgs args)
+	{
+		
+	}
+
+	protected override void OnSelectExited(SelectExitEventArgs args)
+	{
+
+	}
+
+	protected override void OnActivated(ActivateEventArgs args)
+	{
+
+	}
+
+
+}
