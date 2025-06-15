d964c4b03f60405827466c26f1eb36b021ebf840
Clean18 <cleanage95@gmail.com>
2025-06-16 08:59:18 +0900

    Feat : 볼에 콜라이더 닿을 시 UI 조정
    UI 매니저에서 활성화 > 볼에서 비활성화

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+	private bool isHit;
+
+	void OnEnable() => isHit = false;
+
+	void OnDisable() => isHit = false;
+
+	void OnTriggerEnter(Collider other) => Hit();
+
+	void OnCollisionEnter(Collision collision) => Hit();
+
+	void Hit()
+	{
+		if (isHit) return;
+		isHit = true;
+		Debug.Log("볼에 뭔가 맞음");
+		UIManager.Instance.OffPitchingSetting();
+	}
+}
